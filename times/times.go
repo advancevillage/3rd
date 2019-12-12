f174b9d250432d2f0c5f4141c0d78de6e2660b22
advancevillage <cugriver@gmail.com>
2019-12-12 21:49:16 +0800

    #richard: times

--- times/times.go
+++ times/times.go
@@ -2,7 +2,6 @@
 package times
 
 import (
-	"github.com/advancevillage/3rd/logs"
 	"time"
 )
 
@@ -10,34 +9,16 @@ const (
 	Layout = "2006-01-02 15:04:05"
 )
 
-type Times struct {
-	logger logs.Logs
-}
-
-func NewTimes(logger logs.Logs) *Times {
-	return &Times{logger:logger}
-}
-
-func (t *Times) Timestamp() int64 {
+func Timestamp() int64 {
 	return time.Now().Unix()
 }
 
-func (t *Times) TimeString() string {
+func TimeString() string {
 	return time.Now().Format(Layout)
 }
 
-func (t *Times) TimeFormatString(layout string) string {
+func TimeFormatString(layout string) string {
 	return time.Now().Format(layout)
-}
-
-func (t *Times) FormatStringToTime(layout string, value string) (timestamp int64) {
-	times, err := time.ParseInLocation(layout, value, time.Local)
-	if err != nil {
-		t.logger.Error(err.Error())
-		return
-	}
-	timestamp = times.Unix()
-	return
 }
 
 
